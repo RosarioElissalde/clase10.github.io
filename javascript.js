19121694c941dbf9713c51e834ee6ff7ee493087
RosarioElissalde <130700778+RosarioElissalde@users.noreply.github.com>
2023-05-18 22:48:18 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let nombre = "Rosario";
+let apellido = "Elissalde";
+console.log (nombre+" "+apellido);
+
+let numero1 = 13
+let numero2 = 7
+let suma = numero1 + numero2;
+console.log (suma);
+
+let sumaYnumero = suma + 10;
+console.log (sumaYnumero);
